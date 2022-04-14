a3ebaf9905f74a4c111b4c3871d5df647fe375c9
Eva S. Poulsen <eva@btech.au.dk>
2022-04-14 19:55:05 +0200

    CarList

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,17 +3,33 @@ import { View, Text } from "react-native";
 import axios from "axios";
 
 class CarList extends Component {
+    constructor() {
+        super();
+
+        this.state = { carList: [] };
+    }
     componentDidMount() {
         axios.get('https://www.btechwebtech.com/givecars.php')
         .then((response) => {
-            console.log(response);
-        })
+            //console.log(response);
+            this.setState({carList: response.data});
+        });
     }
 
+    renderList = () => {
+        return this.state.carList.map((brand) => {
+            return (
+                <View key={brand.model[0].id}>
+                    <Text>{brand.model[0].name}</Text>
+                </View>
+            )
+        })
+    }
     render () {
+        console.log(this.state);
         return (
             <View>
-                <Text>Car List</Text>
+                {this.renderList()}
             </View>
             );
     }
